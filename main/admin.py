ddb24c68861d15d2b6a318c87c7243f066da78e5
ainbek <aibek-igls@hotmail.com>
2022-05-04 12:07:48 +0200

    added admin models

--- main/admin.py
+++ main/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from .models import ToDo
-from .models import ToMeet
+from .models import ToMeet, Goal_for_month
 
 admin.site.register(ToDo)
 
 admin.site.register(ToMeet)
+admin.site.register(Goal_for_month) 
 
--- main/models.py
+++ main/models.py
@@ -15,4 +15,24 @@ class ToMeet(models.Model):
     comment = models.TextField(null=True, blank=True)
     is_closed = models.BooleanField(default=False)
     is_favorite = models.BooleanField(default=False)
+class Goal_for_month(models.Model):
+    MONTHS_CHOICES = [
+    ('1', 'январь'),
+    ('2', 'февраль'),
+    ('3', 'март'),
+    ('4', 'апрель'),
+    ('5', 'май'),
+    ('6', 'июнь'),
+    ('7', 'июль'),
+    ('8', 'август'),
+    ('9', 'сентябрь'),
+    ('10', 'октябрь'),
+    ('11', 'ноябрь'),
+    ('12', 'декабрь')
+]
+    goal = models.CharField(max_length=255)
+    month = models.CharField(max_length=2, choices=MONTHS_CHOICES, default='1',   )
+    difficulty = models.PositiveSmallIntegerField()
+    reason_for_goal = models.TextField()
+
 
