2a971a45689e0ee79e871e8dc9c4581b2bab1c82
ainbek <aibek-igls@hotmail.com>
2022-05-04 10:23:04 +0200

    copied admin models

--- main/models.py
+++ main/models.py
@@ -6,3 +6,13 @@ class ToDo(models.Model):
     text = models.CharField(max_length=100)
     created_at = models.DateField(auto_now_add=True)
     is_closed = models.BooleanField(default=False)
+    is_favorite = models.BooleanField(default=False)
+
+class ToMeet(models.Model):
+    persone = models.CharField(max_length=70)
+    phone_number = models.CharField(max_length=15)
+    date_of_meeting = models.DateTimeField()
+    comment = models.TextField(null=True, blank=True)
+    is_closed = models.BooleanField(default=False)
+    is_favorite = models.BooleanField(default=False)
+
