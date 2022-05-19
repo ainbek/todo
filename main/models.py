aadead37116db96dd51b90a84b4b917c196f7c5b
ainbek <aibek-igls@hotmail.com>
2022-05-19 10:25:51 +0200

    added

--- main/models.py
+++ main/models.py
@@ -1,8 +1,6 @@
 from  django.db import models
 class ToDo(models.Model):
     text = models.CharField(max_length=100)
-    #
-    # phone_number = models.CharField(max_length=15)
     created_at = models.DateTimeField(auto_now_add=True)
     is_closed = models.BooleanField(default=False)
     is_favorite = models.BooleanField(default=False)
--- main/views.py
+++ main/views.py
@@ -10,6 +10,7 @@ def test(request):
     return render(request, "test.html", {"todo_list": todo_list})
 
 
+
 def add_todo(request):
     form = request.POST
     text = form["todo_text"]
@@ -26,53 +27,55 @@ def fav_todo(request, id):
     todo =ToDo.objects.get(id=id)
     todo.is_favorite = True
     todo.save()
-    return request(test)
+    return redirect(test)
 
 def unfav_todo(request, id):
     todo =ToDo.objects.get(id=id)
     todo.is_closed = True
     todo.save()
-    return request(test)
+    return redirect(test)
 
 def close_todo(request, id):
     todo = ToDo.objects.get(id=id)
     todo.is_closed = not todo.is_closed 
     todo.save()
-    return request(test)
-
-
-def test2(request):
-    item_list = ToMeet.objects.all()
-    return render(request, "meeting.html", {"item_list": item_list}) 
-def second(request):
-    return HttpResponse ("test2  github page")
-
-def add_todo(request):
-    form = request.POST
-    text = form["tomeet_text"]
-    todo = ToMeet(text=text)
-    todo.save()
     return redirect(test)
 
-def delete_todo(request, id):
-    todo = ToMeet.objects.get(id=id)
-    todo.delete()
-    return redirect(test) 
 
-def fav_todo(request, id):
-    todo =ToMeet.objects.get(id=id)
-    todo.is_favorite = True
-    todo.save()
-    return request(test)
 
-def unfav_todo(request, id):
-    todo =ToMeet.objects.get(id=id)
-    todo.is_closed = True
-    todo.save()
-    return request(test)
+def tomeet(request):
+    item_list = ToMeet.objects.all()
+    return render(request, "meeting.html", {"item_list": item_list}) 
+def test2(request):
+    return HttpResponse ("test2  github page")
 
-def close_todo(request, id):
-    todo = ToMeet.objects.get(id=id)
-    todo.is_closed = not todo.is_closed 
-    todo.save()
-    return request(test)
+
+def add_tomeet(request):
+    form = request.POST
+    persone = form["tomeet_text"]
+    tomeet = ToMeet(persone=persone)
+    tomeet.save()
+    return redirect(test2)
+
+def delete_tomeet(request, id):
+    tomeet = ToMeet.objects.get(id=id)
+    tomeet.delete()
+    return redirect(test2) 
+
+def fav_tomeet(request, id):
+    tomeet =ToMeet.objects.get(id=id)
+    tomeet.is_favorite = True
+    tomeet.save()
+    return redirect(test2)
+
+def unfav_tomeet(request, id):
+    tomeet =ToMeet.objects.get(id=id)
+    tomeet.is_closed = True
+    tomeet.save()
+    return redirect(test2)
+
+def close_tomeet(request, id):
+    tomeet = ToMeet.objects.get(id=id)
+    tomeet.is_closed = not tomeet.is_closed 
+    tomeet.save()
+    return redirect(test2)
